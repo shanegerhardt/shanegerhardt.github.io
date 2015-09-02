4d0586c3a854145df561926837b089e0f40aee06
Shane Gerhardt <shane@shanes-mbp.attlocal.net>
2015-09-01 23:45:41 -0700

    Added new site files
    Replacing old jekyll site with new static html implementing google
    material design guidelines

--- js/cover.js
+++
@@ -1,9 +0,0 @@
-var images = ['chalkboard','forest','imac','macbook','mouse','notes','post','sunset'];
-
-function randomNumber(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-$(function() {
-    $('.site-wrapper:first').addClass(images[randomNumber(0, images.length - 1)]).addClass('loaded');
-});
