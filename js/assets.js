fc8b36fe793688d60ac569010d64cbb74f0445e0
Shane Gerhardt <shane.gerhardt@gmail.com>
2016-07-26 17:33:06 -0700

    Loading assets in with javascript to avoid page blocking
    Loading the majority of assets in with javascript. included the
    important css inline.

---
+++ js/assets.js
@@ -0,0 +1,11 @@
+function loadCSS(file){
+  var head  = doc.getElementsByTagName('head')[0];
+  var css  = doc.createElement('link');
+  css.rel  = 'stylesheet';
+  css.type = 'text/css';
+  css.href = file;
+  css.media = 'all';
+  head.appendChild(css);
+}
+loadCSS(doc.location.host + 'css/main.css');
+loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
