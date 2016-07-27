dd838004c1b632e3f52673dd718307bdfdc0ff6c
Shane Gerhardt <shane.gerhardt@gmail.com>
2016-07-26 17:34:50 -0700

    fixing asset url and wrapped critical css in style tags

--- js/assets.js
+++ js/assets.js
@@ -7,5 +7,5 @@ function loadCSS(file){
   css.media = 'all';
   head.appendChild(css);
 }
-loadCSS(doc.location.host + 'css/main.css');
+loadCSS('//' + doc.location.host + 'css/main.css');
 loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
